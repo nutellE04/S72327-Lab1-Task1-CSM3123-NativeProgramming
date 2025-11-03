e73a342cfad941bcf610cf2d2b71da1c164e54cb
Haziq <irfanhaziq321@gmail.com>
2025-11-03 18:08:59 +0800

    feat: Initial project setup
    Adds the basic structure for a new Android application.
    
    This includes:
    - Gradle configuration files (`build.gradle.kts`, `settings.gradle.kts`, `gradle.properties`, and wrapper).
    - Core application manifest (`AndroidManifest.xml`) and resources such as strings, colors, themes, and launcher icons.
    - A main activity with a basic layout displaying profile information.
    - Standard project files for VCS (`.gitignore`) and IDE settings.

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "S72327 Lab 1 Task 1 CSM3123 Native Programming"
+include(":app")
